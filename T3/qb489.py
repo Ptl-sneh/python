32af43dd6a8da192dda00b41b7dcc7d26645f546
Ptl-sneh <ptlsneh1310@gmail.com>
2024-12-15 23:20:34 +0530

    changes in func

--- T3/qb489.py
+++ T3/qb489.py
@@ -1,9 +1,10 @@
 # Write a function cust_data() to ask user to enter their names and age to store data in customer.txt file.
 
-def cust_data(name , age):
+def cust_data():
     f1 = open("E:\python\PYTHON\T3\Text Files\qb489.txt","a")
+    name = input("Enter your name: ")
+    age = input("Enter your age: ")
     f1.write("name: " + name +" age: " +age + "\n")
+    f1.close()
 
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-cust_data(name,age)
+cust_data()
