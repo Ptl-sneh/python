fd21aa96855fbe1c478fe04b7dcc738fc911f17f
Ptl-sneh <ptlsneh1310@gmail.com>
2024-12-15 23:02:46 +0530

    f

---
+++ T3/qb489.py
@@ -0,0 +1,6 @@
+# Write a function cust_data() to ask user to enter their names and age to store data in customer.txt file.
+
+f1 = open("E:\python\PYTHON\T3\Text Files\qb489.txt","a")
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+f1.write("name: " + name +" age: " +age + "\n")
